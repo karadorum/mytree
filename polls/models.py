c45e2eb77bb3fbe132a5df6853ab592359af3be3
Mikhail Borodin <mborodin87@gmail.com>
2018-10-26 18:02:19 +0300

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
+    """
+    вот эта модель должна быть построена по иерархии. Нашел репозиторий который как раз решает эту задачу https://bitbucket.org/larsyencken/django-hierarchy/wiki/Home
+    он не очень легкий, но разбирая его по частям, можно попробовать повторить + добавить templatetags
+    """
     title = models.CharField(max_length=200)
 
 class Product(models.Model):
